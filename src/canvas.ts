e9f4cad1e4a5a43bcddfdf6792590a2abbb40e8d
bennysan <63780263+bennysan@users.noreply.github.com>
2023-09-15 20:16:07 +0200

    Revert "Timeline working... needs polishing"
    This reverts commit 7e1c0734602d203f1672b088a0f88abfaf5d8900.

--- src/canvas.ts
+++ src/canvas.ts
@@ -1,70 +1,25 @@
 import { Shape } from "./shapes";
 import Rectangle from "./shapes/rectangle";
-import { drawPointer } from "./utils/basic-drawing/draw";
-import { lerp } from "./utils/lerp";
 import Vector2 from "./utils/vector2";
 
 class Canvas {
   private canvas: HTMLCanvasElement;
   private ctx: CanvasRenderingContext2D;
   private elements: Shape[];
-
-  public mousePosition: Vector2;
-  public pointerPosition: Vector2;
-
   constructor(root: HTMLElement) {
     this.canvas = document.createElement("canvas");
     this.ctx = this.canvas.getContext("2d");
     this.setDimensions(window.innerWidth, window.innerHeight);
     this.handleWindowResize();
     root.appendChild(this.canvas);
-
     this.elements = [];
-    this.mousePosition = new Vector2(
-      this.canvas.width / 2,
-      this.canvas.height / 2
+    this.addElement(
+      new Rectangle(
+        new Vector2(this.canvas.width / 2, this.canvas.height / 2),
+        100,
+        100
+      )
     );
-    this.pointerPosition = new Vector2(
-      this.mousePosition.x,
-      this.mousePosition.y
-    );
-
-    this.canvas.addEventListener("pointermove", (e) => {
-      this.mousePosition.set(e.clientX, e.clientY);
-    });
-    // const w = 1.5,
-    //   h = 50,
-    //   s = 12 + w,
-    //   radius = (24 * s) / 2,
-    //   count = Math.floor((Math.PI * radius) / s),
-    //   comOrigin = new Vector2(this.canvas.width / 2, this.canvas.height * 0.9);
-    // for (let i = 0; i <= count; i++) {
-    //   const isJan = i % 12 == 0,
-    //     t = i / count;
-
-    //   const rect = new Rectangle(
-    //     new Vector2(
-    //       comOrigin.x - Math.cos(lerp(0, Math.PI, t)) * radius,
-    //       comOrigin.y - Math.sin(lerp(0, Math.PI, t)) * radius
-    //     ),
-    //     isJan ? h : h / 1.5,
-    //     w
-    //   );
-
-    //   rect.rotate(
-    //     Math.atan2(rect.position.y - comOrigin.y, rect.position.x - comOrigin.x)
-    //   );
-
-    //   if (i > count / 2) {
-    //     rect.setFillColor(`rgba(255,255,255,${lerp(0.01, 1, (1 - t) * 3)}`);
-    //   }
-    //   if (i < count / 2) {
-    //     rect.setFillColor(`rgba(255,255,255,${lerp(0.01, 1, t * 3)}`);
-    //   }
-
-    //   rect.setOrigin(0, 0.5).build();
-    //   this.addElement(rect);
-    // }
   }
 
   private addElement(newElemnt: Shape) {
@@ -84,20 +39,11 @@ class Canvas {
     return this;
   }
 
-  public update() {
-    this.elements.forEach((element: Shape) => element.update());
-
-    const t = 0.1;
-    const _x = lerp(this.pointerPosition.x, this.mousePosition.x, t);
-    const _y = lerp(this.pointerPosition.y, this.mousePosition.y, t);
-    this.pointerPosition.set(_x, _y);
-  }
+  public update() {}
 
   public draw(): Canvas {
-    const t = 0.5;
-    this.clear();
-    this.elements.forEach((element: Shape) => element.draw(this.getContext()));
-    drawPointer(this.ctx, this.pointerPosition);
+    this.ctx.fillStyle = "red";
+    this.ctx.fillRect(10, 10, 50, 50);
     return this;
   }
 
